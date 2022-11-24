8e0f5da5b8ec6c581a1bb69580c1805bc4bbb1f5
resttake <resttake@naver.com>
2022-11-24 14:17:53 +0900

    [Unit1] ComposeArticle

--- Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ Unit1/Pathway3/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -24,12 +26,29 @@ import com.example.composearticle.ui.theme.ComposeArticleTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent {
+            ComposeArticleTheme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ComposeArticleApp()
+                }
+            }
+        }
     }
 }
 
 @Composable
-fun ComposeArticleApp() { }
+fun ComposeArticleApp() {
+    ArticleCard(
+        stringResource(R.string.title_jetpack_compose_tutorial),
+        stringResource(R.string.compose_short_desc),
+        stringResource(R.string.compose_long_desc),
+        painterResource(R.drawable.bg_compose_background),
+        Modifier.wrapContentHeight(Alignment.Top)
+    )
+}
 
 @Composable
 private fun ArticleCard(
@@ -39,10 +58,39 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+    Column () {
+        Image(
+            painter = imagePainter,
+            contentDescription = null,
+            modifier = modifier
+        )
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier
+                .padding(start =  16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier
+                .padding(start =  16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier
+                .padding(start =  16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ComposeArticleTheme() {
+        ComposeArticleApp()
+    }
+}
